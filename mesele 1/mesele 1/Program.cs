6aa7232647c7d40f47aa93f3bcd87250b4e010e8
resqa <resul1965qurbanov@gmail.com>
2022-11-01 23:33:56 +0400

    tapsirig1

---
+++ mesele 1/mesele 1/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] reqemler = { -7, -12, 9, 48, 36, 24, 12 };
+int cem = 0;
+
+for (int i = 0; i < reqemler.Length; i++)
+{
+    if(reqemler[i] > 0 && reqemler [i] % 6 == 0)
+{
+    Console.WriteLine(reqemler[i]);
+    cem += reqemler[i];
+}
+}
+Console.WriteLine("toplami " +cem);
